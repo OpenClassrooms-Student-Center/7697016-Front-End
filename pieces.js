39fad39920b98f1b2df68785820245ca81ef6488
Thomas Kerbrat <kerbrat@outlook.com>
2022-05-05 21:06:18 +0200

    Mise à jour de l'affichage après filtrage

--- pieces.js
+++ pieces.js
@@ -2,49 +2,56 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-// Création des fiches produits
-for (let i = 0; i < pieces.length; i++) {
-	// Récupération de l'élément du DOM qui accueillera les fiches
-	const sectionFiches = document.querySelector(".fiches");
+// Fonction qui génère toute la page web
+function genererPieces(pieces) {
 
-	// Création d’une balise dédiée à une pièce automobile
-	const pieceElement = document.createElement("article");
+	// Création des fiches produits
+	for (let i = 0; i < pieces.length; i++) {
+		// Récupération de l'élément du DOM qui accueillera les fiches
+		const sectionFiches = document.querySelector(".fiches");
 
-	// On crée l’élément img.
-	const imageElement = document.createElement("img");
-	// On accède à l’indice i de la liste pieces pour configurer la source de l’image.
-	imageElement.src = pieces[i].image;
-	// On rattache l’image à pieceElement (la balise article)
-	pieceElement.appendChild(imageElement);
+		// Création d’une balise dédiée à une pièce automobile
+		const pieceElement = document.createElement("article");
 
-	// Idem pour le nom, le prix et la catégorie ...
+		// On crée l’élément img.
+		const imageElement = document.createElement("img");
+		// On accède à l’indice i de la liste pieces pour configurer la source de l’image.
+		imageElement.src = pieces[i].image;
+		// On rattache l’image à pieceElement (la balise article)
+		pieceElement.appendChild(imageElement);
 
-	const nomElement = document.createElement("h2");
-	nomElement.innerText = pieces[i].nom;
-	pieceElement.appendChild(nomElement);
+		// Idem pour le nom, le prix et la catégorie ...
 
-	const prixElement = document.createElement("p");
-	prixElement.innerText = "Prix : " + pieces[i].prix + " €"
-		+ " ("
-		+ (pieces[i].prix < 35 ? "€" : "€€€")
-		+ ")";
-	pieceElement.appendChild(prixElement);
+		const nomElement = document.createElement("h2");
+		nomElement.innerText = pieces[i].nom;
+		pieceElement.appendChild(nomElement);
 
-	const categorieElement = document.createElement("p");
-	categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
-	pieceElement.appendChild(categorieElement);
+		const prixElement = document.createElement("p");
+		prixElement.innerText = "Prix : " + pieces[i].prix + " €"
+			+ " ("
+			+ (pieces[i].prix < 35 ? "€" : "€€€")
+			+ ")";
+		pieceElement.appendChild(prixElement);
 
-	const descriptionElement = document.createElement("p");
-	descriptionElement.innerText = pieces[i].description ?? "Pas de description pour le moment.";
-	pieceElement.appendChild(descriptionElement);
+		const categorieElement = document.createElement("p");
+		categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
+		pieceElement.appendChild(categorieElement);
 
-	const disponibiliteElement = document.createElement("p");
-	disponibiliteElement.innerText = pieces[i].disponibilite ? "En stock" : "Rupture de stock";
-	pieceElement.appendChild(disponibiliteElement);
+		const descriptionElement = document.createElement("p");
+		descriptionElement.innerText = pieces[i].description ?? "Pas de description pour le moment.";
+		pieceElement.appendChild(descriptionElement);
 
-	// On rattache la balise article à la section fiches
-	sectionFiches.appendChild(pieceElement);
+		const disponibiliteElement = document.createElement("p");
+		disponibiliteElement.innerText = pieces[i].disponibilite ? "En stock" : "Rupture de stock";
+		pieceElement.appendChild(disponibiliteElement);
+
+		// On rattache la balise article à la section fiches
+		sectionFiches.appendChild(pieceElement);
+	}
 }
+
+// Premier affichage de la page
+genererPieces(pieces);
 
 // Ajout du listener pour trier les pièces par ordre de prix croissant
 const boutonTrier = document.querySelector(".btn-trier");
@@ -53,7 +60,10 @@ boutonTrier.addEventListener("click", function () {
 	piecesReordonnees.sort(function (a, b) {
 		return a.prix - b.prix;
 	});
-	console.log(piecesReordonnees);
+
+	// Effacement de l'écran et regénération de la page
+	document.querySelector(".fiches").innerHTML = "";
+	genererPieces(piecesReordonnees);
 });
 
 // Ajout du listener pour filter les pièces non abordables
@@ -62,7 +72,10 @@ boutonFiltrer.addEventListener("click", function () {
 	const piecesFiltrees = pieces.filter(function (piece) {
 		return piece.prix <= 35;
 	});
-	console.log(piecesFiltrees);
+
+	// Effacement de l'écran et regénération de la page avec les pieces filtrées uniquement
+	document.querySelector(".fiches").innerHTML = "";
+	genererPieces(piecesFiltrees);
 });
 
 // Ajout du listener pour trier les pièces par ordre de prix décroissant
@@ -73,7 +86,8 @@ boutonDecroissant.addEventListener("click", function () {
 		// B - A (et pas A - B)
 		return b.prix - a.prix;
 	});
-	console.log(piecesReordonnees);
+	document.querySelector(".fiches").innerHTML = "";
+	genererPieces(piecesReordonnees);
 });
 
 // Ajout du listener pour filter les pièces sans description
@@ -82,7 +96,8 @@ boutonNodesc.addEventListener("click", function () {
 	const piecesFiltrees = pieces.filter(function (piece) {
 		return Boolean(piece.description);
 	});
-	console.log(piecesFiltrees);
+	document.querySelector(".fiches").innerHTML = "";
+	genererPieces(piecesFiltrees);
 });
 
 // Récupération du nom des pièces
