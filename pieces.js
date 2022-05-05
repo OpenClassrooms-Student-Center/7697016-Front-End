827a40cc40f62f01e13e79cdc2d36c7287327720
Thomas Kerbrat <kerbrat@outlook.com>
2022-05-05 18:45:54 +0200

    Génération d'une fiche produit

---
+++ pieces.js
@@ -0,0 +1,30 @@
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch("pieces-autos.json");
+const pieces = await reponse.json();
+
+// Création des éléments et remplissage du texte ou de la source de l'image
+const ampoule = pieces[0];
+
+const imageElement = document.createElement("img");
+imageElement.src = ampoule.image;
+
+const nomElement = document.createElement("h2");
+nomElement.innerText = ampoule.nom;
+
+const prixElement = document.createElement("p");
+prixElement.innerText = "Prix : " + ampoule.prix + " €"
+	+ " ("
+	+ (ampoule.prix < 35 ? "€" : "€€€")
+	+ ")";
+
+const categorieElement = document.createElement("p");
+categorieElement.innerText = ampoule.categorie ?? "(aucune catégorie)";
+
+// Récupération de l'élément du DOM qui accueillera les fiches
+const sectionFiches = document.querySelector(".fiches");
+
+// Ajout des éléments créés dans le DOM
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiches.appendChild(categorieElement);
