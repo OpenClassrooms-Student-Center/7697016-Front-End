ac3dc48dd2a1f30d1e93a2caf83a033cfb856f90
Thomas Kerbrat <kerbrat@outlook.com>
2022-06-27 21:32:16 +0200

    Récupération des avis

--- avis.js
+++ avis.js
@@ -6,11 +6,24 @@ export function ajoutListenersAvis() {
 	const piecesElements = document.querySelectorAll(".fiches article button");
 
 	for (let i = 0; i < piecesElements.length; i++) {
-		piecesElements[i].addEventListener("click", function (event) {
+		piecesElements[i].addEventListener("click", async function (event) {
 			// Récupération de la valeur de l'attribut data-id="XX".
 			const id = event.target.dataset.id;
-			// Envoie de la requête à l'API.
-			fetch("http://localhost:8081/pieces/" + id + "/avis");
+			// Attente de la réponse de l'API.
+			const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
+			// Reconstruction des données en mémoire depuis la réponse au format JSON.
+			const avis = await reponse.json();
+
+			// Récupération de la balise article pour la pièce désirée.
+			const pieceElement = event.target.parentElement;
+			// Création d'une balise p pour regrouper tous les avis.
+			const avisElement = document.createElement("p");
+
+			for (let i = 0; i < avis.length; i++) {
+				avisElement.innerHTML += avis[i].utilisateur + ': ' + avis[i].commentaire + '<br>';
+			}
+
+			pieceElement.appendChild(avisElement);
 		});
 	}
 }
